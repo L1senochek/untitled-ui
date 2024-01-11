3ab16ff181958da4452a9375d7432431c494138e
l1senochek <lisenochek96@gmail.com>
2024-01-11 14:57:29 +0300

    feat: update styles & add reset file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import './App.css'
-
-function App() { 
+function App() {
   return (
     <div>
       <h1>UI KIT</h1>
       <div>wrapper</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
