a916bfefd35f83bb5a2c97bbb704ca04dccba373
l1senochek <lisenochek96@gmail.com>
2024-01-11 17:13:26 +0300

    feat: update styles & ui kit

--- src/pages/UIKit/UIKit.tsx
+++ src/pages/UIKit/UIKit.tsx
@@ -1,10 +1,20 @@
 import styles from './uikit.module.scss';
 
 const UIKit: React.FC = (): JSX.Element => {
+  const arrElements = [<div>fonts</div>, <div>btn</div> , <div>3</div>, <div>4</div>]
+
   return (
     <div className={styles.uikit}>
-      <h1 className={styles.uikit__title}>UI KIT</h1>
-      <div className={styles.uikit__wrapper}></div>
+      <h2 className={styles.uikit__title}>UI KIT</h2>
+      <div className={styles.uikit__wrapper}>
+        {arrElements.map((element) => {
+          return(
+            <div className={styles.uikit__border}>
+              {element}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 };
