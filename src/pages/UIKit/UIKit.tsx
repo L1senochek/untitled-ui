90b1bb8dc97400b8e161a9e1a81d013ddb6a633b
l1senochek <lisenochek96@gmail.com>
2024-01-11 17:26:40 +0300

    feat: update styles & add Btn

--- src/pages/UIKit/UIKit.tsx
+++ src/pages/UIKit/UIKit.tsx
@@ -1,13 +1,13 @@
 import styles from './uikit.module.scss';
+import { uiKitElements } from '@/utils/const/uiKitElements';
 
 const UIKit: React.FC = (): JSX.Element => {
-  const arrElements = [<div>fonts</div>, <div>btn</div> , <div>3</div>, <div>4</div>]
-
+  
   return (
     <div className={styles.uikit}>
       <h2 className={styles.uikit__title}>UI KIT</h2>
       <div className={styles.uikit__wrapper}>
-        {arrElements.map((element) => {
+        {uiKitElements.map((element) => {
           return(
             <div className={styles.uikit__border}>
               {element}
---
+++ src/utils/const/uiKitElements.tsx
@@ -0,0 +1,14 @@
+import Btn from "@/components/Btn/Btn";
+
+export const uiKitElements = [
+<>
+<h2>fonts:</h2>
+<div>fonts</div>
+</>,
+<>
+  <h2>button:</h2>
+  <Btn>btn</Btn>
+</>, 
+<div>3</div>,
+<div>4</div>
+];
