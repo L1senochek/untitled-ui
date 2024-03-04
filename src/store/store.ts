1a1ca3448837ae54cb00d3664f1ce929211cbcc4
l1senochek <lisenochek96@gmail.com>
2024-03-04 11:41:41 +0300

    feat: add store & hooks

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+export const store = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof store>;
+export type AppDispatch = AppStore['dispatch'];
+export default store;
