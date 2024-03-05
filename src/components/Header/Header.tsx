c7e0e57c2d77e841695e41a3baa56d8203223489
l1senochek <lisenochek96@gmail.com>
2024-03-05 12:27:31 +0300

    feat: add log in & sign up/out btns & provider redux store

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Logo from '@/components/Logo/Logo';
 import Navigation from '@/components/Navigation/Navigation';
+import Btn from '@/components/Btn/Btn';
 import styles from './header.module.scss';
-import { Link } from 'react-router-dom';
-import { INITIAL_PATH } from '@/utils/const/const';
+import { Link, useNavigate } from 'react-router-dom';
+import { INITIAL_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from '@/utils/const/const';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { selectAuth, setAuth } from '@/store/slices/authSlice/authSlice';
 
 const Header: React.FC = (): JSX.Element => {
+  const isAuth = useAppSelector(selectAuth);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const signOutClick = () => {
+    dispatch(setAuth(false));
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.header__leftside}>
@@ -14,7 +25,35 @@ const Header: React.FC = (): JSX.Element => {
         </Link>
         <Navigation />
       </div>
-      <div className={styles.header__rightside}></div>
+      <div className={styles.header__rightside}>
+        {!isAuth ? (
+          <>
+            <Btn
+              variantBtn="tertiary"
+              className={styles.header__btn}
+              onClick={() => navigate(SIGN_IN_PATH)}
+              outlined
+            >
+              Log In
+            </Btn>
+            <Btn
+              variantBtn="tertiary"
+              className={styles.header__btn}
+              onClick={() => navigate(SIGN_UP_PATH)}
+            >
+              Sign Up
+            </Btn>
+          </>
+        ) : (
+          <Btn
+            variantBtn="tertiary"
+            className={styles.header__btn}
+            onClick={signOutClick}
+          >
+            Sign Out
+          </Btn>
+        )}
+      </div>
     </header>
   );
 };
