02fbdaaa6d1a73cab5d7f13f29eb1b23f2b7190d
l1senochek <lisenochek96@gmail.com>
2024-03-06 09:48:24 +0300

    feat: add closing burgerMenu when changing the pathname

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './burger-menu.module.scss';
 import Navigation from '@/components/Navigation/Navigation';
 import HeaderNavigationBtn from '../HeaderNavigationBtn/HeaderNavigationBtn';
+import { useLocation } from 'react-router';
 
 const BurgerMenu: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const location = useLocation();
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const toggleMenu = (): void => setIsMenuOpen(!isMenuOpen);
+
+  const handleClickOutside = (event: MouseEvent): void => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
   };
 
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return (): void => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
+
   return (
-    <div className={styles.burgermenu}>
+    <div className={styles.burgermenu} ref={menuRef}>
       <div className={styles.burgermenu__icon} onClick={toggleMenu}>
         <span
           className={`${styles.burgermenu__bar} ${
