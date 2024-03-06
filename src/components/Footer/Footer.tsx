d2b8c46035dcdbf956eac68719f7128358daabf9
l1senochek <lisenochek96@gmail.com>
2024-03-06 16:02:54 +0300

    feat: add appStore actions / links & styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import FooterSocial from '@/components/FooterSocial/FooterSocial';
+import AppStoreActionsLinks from '@/components/AppStoreActionsLinks/AppStoreActionsLinks';
 
 const Footer: React.FC = (): JSX.Element => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__container}>1</div>
-      <div className={styles.footer__container}>2</div>
+      <div className={styles.footer__container}>
+        <AppStoreActionsLinks />
+      </div>
       <div className={styles.footer__container}>
         <FooterSocial />
       </div>
