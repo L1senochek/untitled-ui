a97e52dbefb69e9a2faa87881f250f19f70477d5
l1senochek <lisenochek96@gmail.com>
2024-04-09 16:09:56 +0300

    feat: add NavigateButtons & styles

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router-dom';
 import { INITIAL_PATH } from '@/utils/const/const';
 import styles from './notfound.module.scss';
-import BackButton from './BackButton/BackButton';
+import ContentSubheading from '@/components/ContentSubheading/ContentSubheading';
+import NavigateButtons from './NavigateButtons/NavigateButtons';
 
 const NotFound: React.FC = (): JSX.Element => {
   return (
     <div className={styles.notfound}>
-      <h2 className={styles.notfound__title}>Page not found!</h2>
-      <h2 className={styles.notfound__message}>404</h2>
-      <Link className={`${styles.notfound__btn} btn`} to={INITIAL_PATH}>
-        Home
-      </Link>
-      <BackButton />
+      <div className={styles.notfound__leftside}>
+        <ContentSubheading
+          subheading="404 error"
+          heading="Page not found"
+          text="Sorry, the page you are looking for doesn't exist.
+          Here are some helpful links:"
+        />
+        <NavigateButtons />
+      </div>
+      <div className={styles.notfound__rightside}>
+        <h2 className={styles.notfound__title}>Page not found!</h2>
+        <h2 className={styles.notfound__message}>404</h2>
+        <Link className={`${styles.notfound__btn} btn`} to={INITIAL_PATH}>
+          Home
+        </Link>
+      </div>
     </div>
   );
 };
