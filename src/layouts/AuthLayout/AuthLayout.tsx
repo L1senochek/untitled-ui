0fc347980e3c8a1a52881535bf2c74539d46b502
l1senochek <lisenochek96@gmail.com>
2024-04-12 12:19:17 +0300

    feat: add styles & element for sign up path

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styles from './authlayout.module.scss';
 import Logo from '@/components/Logo/Logo';
-import { INITIAL_PATH } from '@/utils/const/const';
+import { INITIAL_PATH, SIGN_UP_PATH } from '@/utils/const/const';
+import IconEmail from '@/components/Icons/IconEmail/IconEmail';
 
 const AuthLayout: React.FC = (): JSX.Element => {
+  const location = useLocation();
   return (
     <>
       <header className={styles.header}>
@@ -21,6 +23,12 @@ const AuthLayout: React.FC = (): JSX.Element => {
       <footer className={styles.footer}>
         <div className={styles.footer__leftside}>
           <p>© Untitled UI 2077</p>
+          {location.pathname === SIGN_UP_PATH && (
+            <div className={styles.footer__leftside_email}>
+              <IconEmail />
+              <p>help@untitledui.com</p>
+            </div>
+          )}
         </div>
         <div className={styles.footer__rightside}></div>
       </footer>
