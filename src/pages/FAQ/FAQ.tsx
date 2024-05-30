b10a5fbb55154e59b77b5282dcee211f25ef3b4e
l1senochek <lisenochek96@gmail.com>
2024-05-30 17:31:14 +0300

    feat: update Faq component

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,10 +1,22 @@
+import ContentSubheading from '@/components/ContentSubheading/ContentSubheading';
 import styles from './faq.module.scss';
+import Input from '@/components/Input/Input';
 
 const Faq: React.FC = (): JSX.Element => {
   return (
-    <>
-      <div className={styles.faq}>FAQ</div>
-    </>
+    <div className={styles.faq}>
+      <div className={styles.faq__header}>
+        <ContentSubheading
+          subheading="FAQs"
+          heading="Ask us anything"
+          text="Need something cleared up? Here are our most frequently asked questions."
+        />
+        <Input type="search" placeholder="Search" />
+      </div>
+      <div className={styles.faq__section_faq}>FAQ</div>
+      <div className={styles.faq__section_team}>team</div>
+      <div className={styles.faq__section_cta}>news letter cta</div>
+    </div>
   );
 };
 
