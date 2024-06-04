b8a7f70f2dc99190e2219127623a4c2ef790234d
l1senochek <lisenochek96@gmail.com>
2024-06-04 12:00:07 +0300

    feat: add image NewsLetterCTA & update styles

--- src/components/Faq/NewsLetterCTA/NewsLetterCTA.tsx
+++ src/components/Faq/NewsLetterCTA/NewsLetterCTA.tsx
@@ -6,6 +6,8 @@ import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { schemaEmail } from '@/utils/validation/schema';
 import { yupResolver } from '@hookform/resolvers/yup';
 import IFaqFormData from '@/model/pages/Faq/faqFormData';
+import HoverArrow from '@/components/HoverArrow/HoverArrow';
+import imgNewsLetterCTA from '@/assets/img/faq/newsLetterCTA.png';
 
 const NewsLetterCTA: React.FC = (): JSX.Element => {
   const methods = useForm<IFaqFormData>({
@@ -48,6 +50,15 @@ const NewsLetterCTA: React.FC = (): JSX.Element => {
           <Btn variantBtn="secondary">Subscribe</Btn>
         </form>
       </FormProvider>
+      <div className={styles.newsletter__emailcapture_cta}>
+        <HoverArrow text="Olivia Rhye" />
+        <img
+          className={styles.newsletter__emailcapture_img}
+          src={imgNewsLetterCTA}
+          alt="NewsLetter CTA"
+          loading="lazy"
+        />
+      </div>
     </div>
   );
 };
